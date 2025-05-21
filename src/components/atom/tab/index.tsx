821ba79d9f61fef8c9da13008707dd03710ad725
smd995 <smd99540@gmail.com>
2025-05-21 11:26:22 +0900

    feature/tabs

---
+++ src/components/atom/tab/index.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import { SetStateAction } from "react";
+
+export type Tab = {
+  label: string;
+  value: string;
+};
+
+interface TabsProps {
+  tabs: Tab[];
+  selectedTab: Tab;
+  onChange: (tab: Tab) => void;
+  className?: string;
+}
+
+export const Tabs = ({ tabs, selectedTab, onChange, className }: TabsProps) => {
+  return (
+    <div className={clsx("space-y-1 space-x-3", className)}>
+      {tabs.map((tab) => (
+        <button
+          key={tab.value}
+          className={clsx(
+            tab.value === selectedTab.value
+              ? "text-secondary-900 border-secondary-900 border-b-2"
+              : "text-secondary-200 cursor-pointer",
+          )}
+          onClick={() => onChange(tab)}
+        >
+          {tab.label}
+        </button>
+      ))}
+    </div>
+  );
+};
