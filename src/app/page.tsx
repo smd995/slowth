32e11a1bfefa8ed7efac24dd07652aa39610b2a0
smd995 <smd99540@gmail.com>
2025-05-21 13:50:01 +0900

    feature/tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,3 @@
-"use client";
-
-import { Tabs } from "@/components/atom/tabs";
-import { useState } from "react";
-
 export default function Home() {
-  const [selectedTab, setSelectedTab] = useState({
-    label: "전체",
-    value: "all",
-  });
-  return (
-    <>
-      <Tabs
-        tabs={[
-          { label: "전체", value: "all" },
-          { label: "공지", value: "notice" },
-          { label: "자유글", value: "free" },
-        ]}
-        selectedTab={selectedTab}
-        onChange={setSelectedTab}
-      />
-    </>
-  );
+  return <>slowth 메인페이지입니다.</>;
 }
