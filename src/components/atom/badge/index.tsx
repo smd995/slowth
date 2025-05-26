367112f17870300b2acb37ecdb30bd51c9ac641d
pluminary <hjsdev23@gmail.com>
2025-05-26 19:02:31 +0900

    feature: Badge active 상태 및 hover 스타일 적용 (#61)

--- src/components/atom/badge/index.tsx
+++ src/components/atom/badge/index.tsx
@@ -4,18 +4,20 @@ import clsx from "clsx";
 // 프롭스 인터페이스
 interface BadgeProps {
   count: number; // 표시할 숫자 (0 이하는 렌더링하지 않음)
+  active?: boolean;
 }
 
 // 뱃지 컴포넌트 정의
-export const Badge = ({ count }: BadgeProps) => {
+export const Badge = ({ count, active = false }: BadgeProps) => {
   if (count <= 0) return null;
 
   return (
     <span
       className={clsx(
-        "inline-flex items-center justify-center",
-        "bg-secondary-900 text-xs font-semibold text-white",
-        "h-4 rounded-full px-[7px]",
+        "inline-flex h-4 items-center justify-center rounded-full px-[7px] text-xs transition",
+        active
+          ? "bg-secondary-900 font-semibold text-white"
+          : "group-hover:bg-secondary-900 bg-black/30 font-normal text-white group-hover:font-semibold",
       )}
     >
       {count}
