0d63749958272da05716416e00570f22861f6e01
pluminary <hjsdev23@gmail.com>
2025-05-27 18:44:04 +0900

    style: pageheader 텍스트 스타일 시스템에 맞게 수정 (#67)

--- src/components/molecules/pageHeader/index.tsx
+++ src/components/molecules/pageHeader/index.tsx
@@ -10,10 +10,10 @@ export const PageHeader = () => {
 
       {/* 텍스트 영역 */}
       <div className="flex flex-col justify-center">
-        <p className="text-secondary-700 mb-[8px] text-[14px] font-medium">
+        <p className="text-secondary-700 mb-2 text-sm font-medium">
           함께 할 사람이 없나요?
         </p>
-        <h2 className="text-secondary-900 text-[24px] leading-none font-semibold">
+        <h2 className="text-secondary-900 text-2xl leading-none font-semibold">
           지금 모임에 참여해보세요
         </h2>
       </div>
