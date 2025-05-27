f87befecda8cf268a538e6c2c94e0c19432a17d8
iambean-git <peb1021@naver.com>
2025-05-27 16:06:07 +0900

    chore: 모임 참가자 (avatarList용) 관련 fixture, msw 셋팅 (#70)

--- __tests__/mocks/handler/detail-handlers.ts
+++ __tests__/mocks/handler/detail-handlers.ts
@@ -3,6 +3,7 @@ import {
   gatheringFixture,
   reviewListResponseFixture,
 } from "../../fixtures/detail-fixture";
+import { participantFixture } from "../../fixtures/participants-fixture";
 export const detailHandlers = [
   // 모임 상세 조회
   http.get(
@@ -20,6 +21,14 @@ export const detailHandlers = [
       const offsetString = url.searchParams.get("offset");
       const offset: number = offsetString ? parseInt(offsetString) : 0;
       return HttpResponse.json(reviewListResponseFixture(offset));
+    },
+  ),
+
+  // 특정 모임의 참가자 목록 조회
+  http.get(
+    "https://fe-adv-project-together-dallaem.vercel.app/slotest/gatherings/123/participants",
+    () => {
+      return HttpResponse.json(participantFixture.slice(0, 5));
     },
   ),
 ];
