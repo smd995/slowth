adbafcafe82c1f0dd31cc92765040776eba271d7
iambean-git <peb1021@naver.com>
2025-05-28 18:25:01 +0900

    refactor: 모임 정보 찾을 수 없을 때 에러 처리 404 (#85)

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { BottomFloatingBarWrapper } from "@/components/molecules/bottomFloatingBar/bottomFloatingBarWrapper";
 import { GatheringInformation } from "@/components/molecules/gatheringInformation";
 import { ReviewsWithPagination } from "@/components/organisms/ReviewsWithPagination";
 import {
@@ -6,6 +7,7 @@ import {
 } from "@/effect/gatherings/getGatheringDetail";
 import { getReviews } from "@/effect/reviews/getReviews";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 export default async function Page({
   params,
@@ -16,6 +18,7 @@ export default async function Page({
 
   // 모임 상세 조회
   const gatheringData = await getGatheringDetail(id);
+  if (!gatheringData) return notFound();
   // 특정 모임의 참가자 목록 조회 (avatarList용)
   const participantAvatars = await getParticipants(id);
   // 리뷰 목록 조회
@@ -26,43 +29,46 @@ export default async function Page({
   // console.log("✅ reviewList:", reviewList);
 
   return (
-    <div className="flex h-fit flex-col items-center px-4 sm:px-6">
-      <div className="bg-secondary-50 flex h-full min-h-screen w-full max-w-[1200px] flex-col items-center px-4 pt-6 sm:px-6 sm:pt-6.5 md:pt-10">
-        {/* 이미지 및 상세정보 */}
-        <div className="grid w-full max-w-[996px] grid-rows-2 gap-4 sm:grid-cols-2 sm:grid-rows-1 md:gap-6">
-          <div className="relative h-full w-full overflow-hidden rounded-3xl">
-            <Image
-              src={gatheringData.image}
-              alt={`image-${gatheringData.name}`}
-              fill
-              sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 50vw"
-              priority
-              className="object-cover"
+    <>
+      <div className="flex h-fit flex-col items-center px-4 sm:px-6">
+        <div className="bg-secondary-50 flex h-full min-h-screen w-full max-w-[1200px] flex-col items-center px-4 pt-6 sm:px-6 sm:pt-6.5 md:pt-10">
+          {/* 이미지 및 상세정보 */}
+          <div className="grid w-full max-w-[996px] grid-rows-2 gap-4 sm:grid-cols-2 sm:grid-rows-1 md:gap-6">
+            <div className="relative h-full w-full overflow-hidden rounded-3xl">
+              <Image
+                src={gatheringData.image}
+                alt={`image-${gatheringData.name}`}
+                fill
+                sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 50vw"
+                priority
+                className="object-cover"
+              />
+            </div>
+            <GatheringInformation
+              gatheringInfo={gatheringData}
+              participantAvatars={participantAvatars}
             />
           </div>
-          <GatheringInformation
-            gatheringInfo={gatheringData}
-            participantAvatars={participantAvatars}
-          />
-        </div>
-        {/* 리뷰 */}
-        <div className="border-t-secondary-200 mt-4 flex w-full max-w-[996px] grow flex-col border-t-2 bg-white px-7 pt-7 pb-28 sm:mt-5 md:mt-6">
-          <p className="text-secondary-900 h-fit font-semibold sm:text-lg">
-            이용자들은 이 프로그램을 이렇게 느꼈어요!
-          </p>
-          {!reviewList || reviewList.totalItemCount === 0 ? (
-            <div className="flex w-full grow items-center justify-center">
-              <p className="text-secondary-500">아직 리뷰가 없어요</p>
-            </div>
-          ) : (
-            <ReviewsWithPagination
-              gatheringId={gatheringData.id}
-              initialReviews={reviewList.data}
-              totalPages={reviewList.totalPages}
-            />
-          )}
+          {/* 리뷰 */}
+          <div className="border-t-secondary-200 mt-4 flex w-full max-w-[996px] grow flex-col border-t-2 bg-white px-7 pt-7 pb-28 sm:mt-5 md:mt-6">
+            <p className="text-secondary-900 h-fit font-semibold sm:text-lg">
+              이용자들은 이 프로그램을 이렇게 느꼈어요!
+            </p>
+            {!reviewList || reviewList.totalItemCount === 0 ? (
+              <div className="flex w-full grow items-center justify-center">
+                <p className="text-secondary-500">아직 리뷰가 없어요</p>
+              </div>
+            ) : (
+              <ReviewsWithPagination
+                gatheringId={gatheringData.id}
+                initialReviews={reviewList.data}
+                totalPages={reviewList.totalPages}
+              />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+      <BottomFloatingBarWrapper gatheringId={parseInt(id)} />
+    </>
   );
 }
--- src/effect/gatherings/getGatheringDetail.ts
+++ src/effect/gatherings/getGatheringDetail.ts
@@ -6,10 +6,24 @@ const teamId = "slotest";
 // 모임 상세 조회
 export const getGatheringDetail = async (id: string) => {
   // 추후 에러 고려하여 404 처리 추가해주기
-  const response = await fetch(`${BASE_URL}/${teamId}/gatherings/${id}`);
-  const data = await response.json();
+  try {
+    const response = await fetch(`${BASE_URL}/${teamId}/gatherings/${id}`);
+    const data = await response.json();
 
-  return data;
+    // 응답이 404 에러인 경우
+    if (response.status === 404 || data?.code === "NOT_FOUND") {
+      return null;
+    }
+
+    // 기타 서버 오류
+    if (!response.ok) {
+      throw new Error(`서버 오류 발생: ${response.status}`);
+    }
+    return data;
+  } catch (error) {
+    console.error("getGatheringDetail 실패:", error);
+    throw error;
+  }
 };
 
 // 특정 모임의 참가자 목록 조회
