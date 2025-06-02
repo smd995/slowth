f105fd1fb72b5e88b1ad326701a7d0d1fb1145c6
iambean-git <peb1021@naver.com>
2025-06-02 19:03:14 +0900

    feature: DeadlineTag 컴포넌트 구현 및 사용 (#111)

--- src/components/molecules/gatheringCard/index.tsx
+++ src/components/molecules/gatheringCard/index.tsx
@@ -12,6 +12,7 @@ import { OpenConfirmedBadge } from "@/components/atom/openConfirmedBadge";
 import { UserIcon } from "@/components/icons/UserIcon";
 import { ArrowRight } from "@/components/icons/ArrowRight";
 import { ByeIcon } from "@/components/icons/ByeIcon";
+import { DeadlineTag } from "@/components/atom/deadlineTag";
 
 export interface GatheringCardProps {
   gathering: Gathering;
@@ -28,7 +29,7 @@ export const GatheringCard = ({
   const isFull = gathering.participantCount >= gathering.capacity;
   const router = useRouter();
   const goToGatheringDetail = () => {
-    router.push(`/detail/${gathering.id}`); // 모임 상세 페이지로 이동
+    router.push(`/gathering/${gathering.id}`); // 모임 상세 페이지로 이동
   };
 
   useEffect(() => {
@@ -57,6 +58,7 @@ export const GatheringCard = ({
           alt={gathering.name}
           className="h-full w-full object-cover"
         />
+        <DeadlineTag registrationEnd={gathering.registrationEnd} />
       </div>
 
       {/* 우측 본문 */}
---
+++ src/effect/date/getDeadlineMessage.ts
@@ -0,0 +1,38 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export const getDeadlineMessage = (
+  registrationEnd: string,
+): {
+  isDeadlineSoon: boolean;
+  message: string;
+} => {
+  const KST = "Asia/Seoul";
+
+  const end = dayjs.utc(registrationEnd).tz(KST);
+  const now = dayjs().tz(KST);
+
+  const endDate = end.format("YYYY-MM-DD");
+  const nowDate = now.format("YYYY-MM-DD");
+  const tomorrowDate = now.add(1, "day").format("YYYY-MM-DD");
+
+  const hour = end.format("H");
+  let message = "";
+  let isDeadlineSoon = false;
+
+  if (endDate === nowDate) {
+    message = `오늘 ${hour}시 마감`;
+    isDeadlineSoon = end.isAfter(now); // 현재 시간 이후일 때만 true
+  } else if (endDate === tomorrowDate) {
+    message = `내일 ${hour}시 마감`;
+    isDeadlineSoon = true;
+  } else {
+    message = "";
+  }
+
+  return { isDeadlineSoon, message };
+};
