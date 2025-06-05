591613afb6c99bc9a519e2a7bf8a9706c57abd1f
iambean-git <peb1021@naver.com>
2025-06-05 18:15:07 +0900

    fix: 상세페이지 api 요청 관련 오류 수정 (axios client, toast 사용) (#117)

--- src/effect/gatherings/cancelGathering.ts
+++ src/effect/gatherings/cancelGathering.ts
@@ -1,31 +1,6 @@
-// 모임 취소하기 (주최자만 가능)
+import client from "../client/client";
+
 export const cancelGathering = async (id: number) => {
-  const token = localStorage.getItem("token");
-
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/${process.env.NEXT_PUBLIC_TEAM_ID}/gatherings/${id}/cancel`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${token}`,
-        },
-      },
-    );
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(errorData.message || "모임 취소에 실패했습니다.");
-    }
-
-    const data = await response.json();
-    // console.log("✅ cancelGathering data:", data);
-
-    if (!data.canceledAt) return false;
-    return true; // 취소 성공시 true 반환환
-  } catch (error) {
-    console.error("모임 취소 중 오류:", error);
-    throw error;
-  }
+  const response = await client.put(`gatherings/${id}/cancel`);
+  return response.data;
 };
--- src/effect/gatherings/getJoinedGatherings.ts
+++ src/effect/gatherings/getJoinedGatherings.ts
@@ -1,6 +1,5 @@
-// 로그인된 사용자가 참석한 모임 목록 조회
-
 import { JoinedGathering } from "@/entity/gathering";
+import client from "../client/client";
 
 interface GetJoinedGatheringsParams {
   completed?: boolean;
@@ -14,7 +13,6 @@ interface GetJoinedGatheringsParams {
 export const getJoinedGatherings = async (
   getJoinedGatheringsParams: GetJoinedGatheringsParams,
 ): Promise<JoinedGathering[]> => {
-  const token = localStorage.getItem("token");
   const params = new URLSearchParams();
 
   Object.entries(getJoinedGatheringsParams).forEach(([key, value]) => {
@@ -22,29 +20,6 @@ export const getJoinedGatherings = async (
       params.append(key, value.toString());
     }
   });
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/${process.env.NEXT_PUBLIC_TEAM_ID}/gatherings/joined`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${token}`,
-        },
-      },
-    );
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(
-        errorData.message || "사용자가 참석한 모임 목록 조회에 실패했습니다.",
-      );
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("사용자가 참석한 모임 목록 조회 중 오류:", error);
-    throw error;
-  }
+  const response = await client.get(`gatherings/joined?${params}`);
+  return response.data;
 };
