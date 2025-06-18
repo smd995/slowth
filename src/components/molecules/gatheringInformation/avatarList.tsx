ddc539489dbf44c33f6544c011bbceca34518ea9
iambean-git <peb1021@naver.com>
2025-06-18 13:43:51 +0900

    refactor: detail with swr (#173)

--- src/components/molecules/gatheringInformation/avatarList.tsx
+++ src/components/molecules/gatheringInformation/avatarList.tsx
@@ -1,5 +1,4 @@
 import { Avatar } from "@/components/atom/avatar";
-import { useEffect, useState } from "react";
 
 export type participantAvatar = {
   id: number;
@@ -16,17 +15,10 @@ export const AvartarList = ({
   participantCount,
   participantAvatars,
 }: AvartarListProps) => {
-  // console.log("✅ participantAvatars:", participantAvatars);
+  const displayparticipantAvatars =
+    participantCount > 5 ? participantAvatars.slice(0, 4) : participantAvatars;
 
-  const [displayparticipantAvatars, setDisaplayparticipantAvatars] =
-    useState(participantAvatars); // 프로필 사진이 보여지는 사용자 리스트
-  const [overNumber, setOverNumber] = useState(0); // 5명 이상일 경우, 숫자로 표기되는 인원 수
-  useEffect(() => {
-    if (participantCount > 5) {
-      setDisaplayparticipantAvatars(participantAvatars.slice(0, 4));
-      setOverNumber(participantCount - 4);
-    }
-  }, [participantAvatars, participantCount]);
+  const overNumber = participantCount > 5 ? participantCount - 4 : 0;
 
   return (
     <div className="flex -space-x-2.5">
@@ -41,7 +33,7 @@ export const AvartarList = ({
 
       {/* 5명 이상일 경우, 숫자로 표기되는 인원 수  */}
       {participantCount > 5 && (
-        <div className="bg-secondary-100 text-secondary-800 flex h-[30px] w-[30px] items-center justify-center rounded-full text-sm font-semibold">
+        <div className="bg-secondary-100 text-secondary-800 z-10 flex h-[30px] w-[30px] items-center justify-center rounded-full text-sm font-semibold">
           +{overNumber}
         </div>
       )}
